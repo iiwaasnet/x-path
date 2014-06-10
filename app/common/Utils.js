32e7f7704c9524ee3b783f50375deb03ce977e88
iiwaasnet <givasena@gmail.com>
2014-06-10 16:47:07 +0200

    - Image crop

---
+++ app/common/Utils.js
@@ -0,0 +1,13 @@
+angular.module('xpa').service('Utils', function(){
+    var service = this
+
+    service.atIndex = function(array, element){
+        var i = a.length;
+        while (i--) {
+            if (a[i] === obj) {
+                return i;
+            }
+        }
+        return -1;
+    };
+});
