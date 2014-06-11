c79501038a2f3c9b7dfb67aec4f1f028081636b2
iiwaasnet <givasena@gmail.com>
2014-06-11 07:11:59 +0200

    - Image crop

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,17 +1,52 @@
-angular.module('xpa').controller('HomeController', ['$document', function ($document) {
-    this.person = new Person();
-    this.person.firstName = 'Frank';
-    this.person.lastName = 'Musterman';
-    this.person.age = 40;
-    this.image = {url: "http://lorempixel.com/720/520/",
+angular.module('xpa').controller('HomeController', ['$document', 'ImageCropProviders',
+    function ($document, ImageCropProviders) {
+    var ctrl = this;
+    ctrl.person = new Person();
+    ctrl.person.firstName = 'Frank';
+    ctrl.person.lastName = 'Musterman';
+    ctrl.person.age = 40;
+    ctrl.image = {url: "http://lorempixel.com/720/520/",
         position: {
             x: 0,
             y: 0
         },
         id: 1};
+    var cropProviders = [ctrl.image.id];
 
-    this.CropImage = function () {
+    ctrl.CropImage = function () {
 //        this.person.lastName = Date();
         console.log(JSON.stringify(this.imagePosition));
     };
+
+    ctrl.crop = function () {
+        angular.forEach(getRegisteredProviders(), function(value){
+            value.crop();
+        });
+    };
+    ctrl.edit = function () {
+        angular.forEach(getRegisteredProviders(), function(value){
+            value.edit();
+        });
+    };
+
+    function getRegisteredProviders() {
+        var providers = [];
+        if (cropProviders.length) {
+            angular.forEach(cropProviders, function (value) {
+                var provider = ImageCropProviders.find(value);
+                if (provider) {
+                    providers.push(provider);
+                }
+            });
+        }
+        else {
+            angular.forEach(ImageCropProviders.all(), function (value) {
+                if (value) {
+                    providers.push(value);
+                }
+            });
+        }
+
+        return providers;
+    }
 }]);
